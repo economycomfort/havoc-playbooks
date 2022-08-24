7302a7492f32f3f84174225f46f6a11a0235c765
Tom D'Aquino <tdaquino@vectra.ai>
2022-08-23 21:37:35 -0700

    Fixing get_module_results function.

--- pse_network_recon/pse_network_recon.py
+++ pse_network_recon/pse_network_recon.py
@@ -98,9 +98,10 @@ def get_module_results(executing_module, module_instruct_instance, module_task_i
             module_results = h.interact_with_task(c2_task_name, instruct_command, module_instruct_instance, instruct_args)
             if module_results['outcome'] == 'success':
                 for module_result in module_results['results']:
-                    if 'taskID' in module_result and module_result['taskID'] == module_task_id:
-                        if module_result['results'] and module_result['results'] is not None:
-                            results = module_result['results']
+                    if 'taskID' in module_result and module_result['taskID'] == module_task_id and 'results' in module_result:
+                        tmp_results = module_result['results']
+                        if tmp_results is not None and 'Job started:' not in tmp_results:
+                            results = tmp_results
             else:
                 results = f'{instruct_command} failed.\n'
             if not results:
